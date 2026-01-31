4ce3f2b32efd63ba9f0e2abdc444f7775ebbafdd
Alex <afizcouceiro@gmail.com>
2026-01-31 01:01:23 +0100

    BookManager

--- CluedosParty/Assets/_Scripts/BookManager.cs
+++ CluedosParty/Assets/_Scripts/BookManager.cs
@@ -1,8 +1,11 @@
+using System.Collections;
 using UnityEngine;
 
 public class BookManager : MonoBehaviour
 {
     public static BookManager Instance { get; private set; }
+    [SerializeField] private float _transitionDuration;
+    [SerializeField] private GameObject[] _panels;
 
     public GameObject[] _notes;
     private void Awake()
@@ -20,6 +23,22 @@ public class BookManager : MonoBehaviour
     public void ActivateNotes(int _note)
     {
         _notes[_note].SetActive(true);
+    }
+
+    public void Transition(GameObject Toactivate)
+    {
+        for(int i = 0; i<_panels.Length; i++)
+        {
+            _panels[i].SetActive(false);
+        }
+        StartCoroutine(ActivateObject(Toactivate));
+    }
+    
+    private IEnumerator ActivateObject(GameObject Toactivate)
+    {
+        yield return new WaitForSeconds(_transitionDuration);
+        Toactivate.SetActive(true);
+
     }
 
 
