68d4fd498bfae17fa3af7452563b22e603d86483
MiguelilloElPillo <mialca134@gmail.com>
2026-01-31 02:58:57 +0100

    Cosicas

--- CluedosParty/Assets/_Scripts/UIManager.cs
+++ CluedosParty/Assets/_Scripts/UIManager.cs
@@ -3,6 +3,31 @@ using UnityEngine;
 public class UIManager : MonoBehaviour
 {
     private bool isGamePaused;
+    [SerializeField] private GameObject noteboard;
 
-    
+    private void Start()
+    {
+        isGamePaused = false;
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void OpenCloseMenu(bool paused)
+    {
+        if (paused)
+        {
+            //Despausar
+            Time.timeScale = 1.0f;
+            
+            
+           
+        }
+    }
+
+
+
 }
